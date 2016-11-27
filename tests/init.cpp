18177748aa40d8914280cd62f019a493b9d05217
anastasiya0304 <89684777770@mail.ru>
2016-11-27 22:26:42 +0300

    Update init.cpp

--- tests/init.cpp
+++ tests/init.cpp
@@ -1,1 +1,37 @@
+#include "sort.hpp"
+#include <catch.hpp>
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <locale> 
+using namespace std;
+ 
+SCENARIO("count111", "[count1111]"){
+ setlocale(LC_ALL, "Russian");
+ Sort obj("name.txt",1048576);
+ ifstream hay("out.txt");
+ifstream file("sort.txt");
 
+int i=0;
+ bool p=true;
+ string s1,s2;
+ while (!hay.eof()&&!file.eof()){
+getline(file,s1);
+ getline(hay,s2);
+
+  i++;
+  if (s1!=s2){
+  p=false;
+   cout<<i<<endl;
+   cout<<s1<<endl;
+   cout<<s2<<endl;
+   break;
+  }
+ }
+  file.close();
+ hay.close();
+ 
+ 
+
+  REQUIRE(p==true);
+}
